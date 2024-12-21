756590d55cc0e89a935def3a79261fc7ada6d811
Varun Krishna. P <Varunkrishna.dbz@gmail.com>
2024-12-21 09:42:53 +0530

    feat: the first mongoose model

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
 import { initDatabase } from './db/init.js'
+import { Post } from './db/models/post.js'
 
-initDatabase()
+await initDatabase()
+
+const post = new Post({
+  title: 'Hello Mongoose!',
+  author: 'Daniel bug1',
+  contents: 'This post is stored in mongoDB database',
+  tags: ['moongose', 'mongodb'],
+})
+
+await post.save()
+
+const posts = await Post.find()
+console.log(posts)
